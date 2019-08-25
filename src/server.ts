87859fc012bd6caadca1b5fa1ddbf46468be7f02
Thomas Borck <mail@bommy.net>
2019-08-25 21:07:01 +0200

    Added 'cors' to handle external requests.

--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,41 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
 
-import Storage from './storage/NodePersist';
+import Storage from "./storage/NodePersist";
 
-import Calendar from './calendar/Calendar';
-import Appointment from './calendar/entities/Appointment';
-import Anniversary from './calendar/entities/Anniversary';
-import { calendarToICal } from './calendar/parser/entityToICal';
+import Calendar from "./calendar/Calendar";
+import Appointment from "./calendar/entities/Appointment";
+import Anniversary from "./calendar/entities/Anniversary";
+import { calendarToICal } from "./calendar/parser/entityToICal";
 
 // prepare globals
 const app = express();
 const port = 22222;
-const iCal = new Calendar('Bommys Kalender', '-//bommynet/pixlcal//NONSGML v1.0-alpha//DE');
+const iCal = new Calendar(
+  "Bommys Kalender",
+  "-//bommynet/pixlcal//NONSGML v1.0-alpha//DE"
+);
 const storage = new Storage();
 
+app.get("/api/calendar/sync", (req, res) => {
+  console.log("Calendar-Sync by", req.ip);
 
-app.get('/api/calendar/sync', (req, res) => {
-    console.log('Calendar-Sync by', req.ip);
+  const iCalString = calendarToICal(iCal);
 
-    const iCalString = calendarToICal(iCal);
-
-    res.set('Content-Type', 'text/calendar;charset=utf-8');
-    res.set('Content-Disposition', 'attachment; filename="calendar.ics"');
-    res.send(iCalString);
+  res.set("Content-Type", "text/calendar;charset=utf-8");
+  res.set("Content-Disposition", 'attachment; filename="calendar.ics"');
+  res.send(iCalString);
 });
-
 
 /**
  * Get all stored appointments.
  * response:
  *  - (200) a list of all appointments
  */
-app.get('/api/calendar/appointment', (req, res) => {
-    console.log('Appointment-ReadAll');
-    res.status(200).send(iCal.appointments);
-})
+app.get("/api/calendar/appointment", cors(), (req, res) => {
+  console.log("Appointment-ReadAll");
+  res.status(200).send(iCal.appointments);
+});
 
 /**
  * Create a new appointment.
@@ -41,18 +43,22 @@ app.get('/api/calendar/appointment', (req, res) => {
  *  - (201) created appointment
  *  - (403) error message
  */
-app.post('/api/calendar/appointment', (req, res) => {
-    console.log('Appointment-Add');
+app.post("/api/calendar/appointment", (req, res) => {
+  console.log("Appointment-Add");
 
-    try {
-        const appointment = iCal.createAppointment(req.query);
-        storage.store(appointment);
-        storage.updateConfig(iCal.nextAlarmId, iCal.nextAnniversaryId, iCal.nextAppointmentId);
-        res.status(201).send(appointment);
-    } catch (reason) {
-        res.status(403).send(reason.message);
-    }
-})
+  try {
+    const appointment = iCal.createAppointment(req.query);
+    storage.store(appointment);
+    storage.updateConfig(
+      iCal.nextAlarmId,
+      iCal.nextAnniversaryId,
+      iCal.nextAppointmentId
+    );
+    res.status(201).send(appointment);
+  } catch (reason) {
+    res.status(403).send(reason.message);
+  }
+});
 
 /**
  * Update an existing appointment.
@@ -61,31 +67,28 @@ app.post('/api/calendar/appointment', (req, res) => {
  *  - (403) id is not valid
  *  - (404) appointment not found
  */
-app.post('/api/calendar/appointment/:id', async (req, res) => {
-    const id = "" + req.params['id'];
-    console.log(`Appointment-Update: ${id}`);
+app.post("/api/calendar/appointment/:id", async (req, res) => {
+  const id = "" + req.params["id"];
+  console.log(`Appointment-Update: ${id}`);
 
-    let responseState = 403;
-    let responseUpdatedObject: Appointment | undefined;
+  let responseState = 403;
+  let responseUpdatedObject: Appointment | undefined;
 
-    if (typeof id === 'string' && id.length > 0) {
-        try {
-            responseUpdatedObject = await iCal.updateAppointment(id, req.query);
+  if (typeof id === "string" && id.length > 0) {
+    try {
+      responseUpdatedObject = await iCal.updateAppointment(id, req.query);
 
-
-            if (responseUpdatedObject) {
-                storage.store(responseUpdatedObject);
-                responseState = 200;
-            }
-            else
-                responseState = 404;
-        } catch (error) {
-            responseState = 500;
-        }
+      if (responseUpdatedObject) {
+        storage.store(responseUpdatedObject);
+        responseState = 200;
+      } else responseState = 404;
+    } catch (error) {
+      responseState = 500;
     }
+  }
 
-    res.status(responseState).send(responseUpdatedObject);
-})
+  res.status(responseState).send(responseUpdatedObject);
+});
 
 /**
  * Delete an existing appointment.
@@ -94,37 +97,34 @@ app.post('/api/calendar/appointment/:id', async (req, res) => {
  *  - (403) id is not valid
  *  - (404) appointment not found
  */
-app.delete('/api/calendar/appointment/:id', (req, res) => {
-    const id: string = "" + req.params['id'];
+app.delete("/api/calendar/appointment/:id", (req, res) => {
+  const id: string = "" + req.params["id"];
 
-    let responseState = 403;
-    let responseDeletedObject: Appointment | undefined;
+  let responseState = 403;
+  let responseDeletedObject: Appointment | undefined;
 
-    if (typeof id === 'string' && id.length > 0) {
-        responseDeletedObject = iCal.removeAppointment(id);
+  if (typeof id === "string" && id.length > 0) {
+    responseDeletedObject = iCal.removeAppointment(id);
 
-        if (responseDeletedObject) {
-            storage.store(responseDeletedObject);
-            responseState = 200;
-        }
-        else
-            responseState = 404;
-    }
+    if (responseDeletedObject) {
+      storage.store(responseDeletedObject);
+      responseState = 200;
+    } else responseState = 404;
+  }
 
-    res.status(responseState).send(responseDeletedObject);
-})
-
+  res.status(responseState).send(responseDeletedObject);
+});
 
 /**
  * Get all stored anniversaries.
  * response:
  *  - (200) a list of all anniversaries
  */
-app.get('/api/calendar/anniversary', (req, res) => {
-    console.log('Anniversary-Read by', req.ip);
+app.get("/api/calendar/anniversary", (req, res) => {
+  console.log("Anniversary-Read by", req.ip);
 
-    res.status(200).send(iCal.anniversaries);
-})
+  res.status(200).send(iCal.anniversaries);
+});
 
 /**
  * Create a new anniversary.
@@ -132,20 +132,24 @@ app.get('/api/calendar/anniversary', (req, res) => {
  *  - (201) created anniversary
  *  - (403) error message
  */
-app.post('/api/calendar/anniversary', (req, res) => {
-    console.log('Anniversary-Add by', req.ip);
+app.post("/api/calendar/anniversary", (req, res) => {
+  console.log("Anniversary-Add by", req.ip);
 
-    try {
-        const anniverary = iCal.createAnniversary(req.query);
+  try {
+    const anniverary = iCal.createAnniversary(req.query);
 
-        storage.store(anniverary);
-        storage.updateConfig(iCal.nextAlarmId, iCal.nextAnniversaryId, iCal.nextAppointmentId);
+    storage.store(anniverary);
+    storage.updateConfig(
+      iCal.nextAlarmId,
+      iCal.nextAnniversaryId,
+      iCal.nextAppointmentId
+    );
 
-        res.status(201).send(anniverary);
-    } catch (reason) {
-        res.status(403).send(reason.message);
-    }
-})
+    res.status(201).send(anniverary);
+  } catch (reason) {
+    res.status(403).send(reason.message);
+  }
+});
 
 /**
  * Update an existing anniversary.
@@ -154,30 +158,28 @@ app.post('/api/calendar/anniversary', (req, res) => {
  *  - (403) id is not valid
  *  - (404) anniversary not found
  */
-app.post('/api/calendar/anniversary/:id', async (req, res) => {
-    const id = "" + req.params['id'];
-    console.log(`Anniversary-Update: ${id}`);
+app.post("/api/calendar/anniversary/:id", async (req, res) => {
+  const id = "" + req.params["id"];
+  console.log(`Anniversary-Update: ${id}`);
 
-    let responseState = 403;
-    let responseUpdatedObject: Anniversary | undefined;
+  let responseState = 403;
+  let responseUpdatedObject: Anniversary | undefined;
 
-    if (typeof id === 'string' && id.length > 0) {
-        try {
-            responseUpdatedObject = await iCal.updateAnniversary(id, req.query);
+  if (typeof id === "string" && id.length > 0) {
+    try {
+      responseUpdatedObject = await iCal.updateAnniversary(id, req.query);
 
-            if (responseUpdatedObject) {
-                storage.store(responseUpdatedObject);
-                responseState = 200;
-            }
-            else
-                responseState = 404;
-        } catch (error) {
-            responseState = 500;
-        }
+      if (responseUpdatedObject) {
+        storage.store(responseUpdatedObject);
+        responseState = 200;
+      } else responseState = 404;
+    } catch (error) {
+      responseState = 500;
     }
+  }
 
-    res.status(responseState).send(responseUpdatedObject);
-})
+  res.status(responseState).send(responseUpdatedObject);
+});
 
 /**
  * Delete an existing anniversary.
@@ -186,56 +188,53 @@ app.post('/api/calendar/anniversary/:id', async (req, res) => {
  *  - (403) id is not valid
  *  - (404) anniversary not found
  */
-app.delete('/api/calendar/anniversary/:id', (req, res) => {
-    const id: string = "" + req.params['id'];
+app.delete("/api/calendar/anniversary/:id", (req, res) => {
+  const id: string = "" + req.params["id"];
 
-    let responseState = 403;
-    let responseDeletedObject: Anniversary | undefined;
+  let responseState = 403;
+  let responseDeletedObject: Anniversary | undefined;
 
-    if (typeof id === 'string' && id.length > 0) {
-        responseDeletedObject = iCal.removeAnniversary(id);
+  if (typeof id === "string" && id.length > 0) {
+    responseDeletedObject = iCal.removeAnniversary(id);
 
-        if (responseDeletedObject) {
-            storage.delete(responseDeletedObject);
-            responseState = 200;
-        }
-        else
-            responseState = 404;
-    }
+    if (responseDeletedObject) {
+      storage.delete(responseDeletedObject);
+      responseState = 200;
+    } else responseState = 404;
+  }
 
-    res.status(responseState).send(responseDeletedObject);
-})
-
+  res.status(responseState).send(responseDeletedObject);
+});
 
 // setup storage
-storage.init()
-    .then((config) => {
-        iCal.nextAppointmentId = config.appointmentId;
-        iCal.nextAnniversaryId = config.anniversaryId;
-        console.log('Storage ready');
-    })
-    .then(() => {
-        console.log('Storage: load all anniversaries');
-        return storage.loadAllAnniversaries();
-    })
-    .then((anniversaries) => {
-        iCal.anniversaries = anniversaries;
-        console.log(`  - ${anniversaries.length} loaded.`);
-        console.log('Storage: load all appointments');
-        return storage.loadAllAppointments();
-    })
-    .then((appointments) => {
-        iCal.appointments = appointments;
-        console.log(`  - ${appointments.length} loaded.`);
-        console.log('Storage: load all alarms');
-        return storage.loadAllAlarms();
-    })
-    .then((alarms) => {
-        iCal.alarms = alarms;
-        console.log(`  - ${alarms.length} loaded.`);
-    })
-    .then(() => {
-        app.listen(port, () => console.log(`Application started at ${port}`));
-    })
-    .catch(console.error);
-
+storage
+  .init()
+  .then(config => {
+    iCal.nextAppointmentId = config.appointmentId;
+    iCal.nextAnniversaryId = config.anniversaryId;
+    console.log("Storage ready");
+  })
+  .then(() => {
+    console.log("Storage: load all anniversaries");
+    return storage.loadAllAnniversaries();
+  })
+  .then(anniversaries => {
+    iCal.anniversaries = anniversaries;
+    console.log(`  - ${anniversaries.length} loaded.`);
+    console.log("Storage: load all appointments");
+    return storage.loadAllAppointments();
+  })
+  .then(appointments => {
+    iCal.appointments = appointments;
+    console.log(`  - ${appointments.length} loaded.`);
+    console.log("Storage: load all alarms");
+    return storage.loadAllAlarms();
+  })
+  .then(alarms => {
+    iCal.alarms = alarms;
+    console.log(`  - ${alarms.length} loaded.`);
+  })
+  .then(() => {
+    app.listen(port, () => console.log(`Application started at ${port}`));
+  })
+  .catch(console.error);
